abe6179a6427d00c24090294dd6fbd891cdc92e9
Ramin <ramin.vakili@firstsource.io>
2019-10-06 13:47:58 +0330

    minor changes

--- lib/model/contract_category.dart
+++ lib/model/contract_category.dart
@@ -6,9 +6,8 @@ class ContractCategory {
 
   ContractCategory(
       {@required ContractsForSymbolResponse contractsForSymbolResponse}) {
-
-    for (Available availableContract in contractsForSymbolResponse.contractsFor.available) {
-
+    for (Available availableContract in contractsForSymbolResponse.contractsFor
+        .available) {
       Category category = _containsCategory(availableContract.contractCategory);
 
       if (category == null) {
@@ -21,7 +20,7 @@ class ContractCategory {
       category.addContractType(available: availableContract);
     }
 
-    for (var c in categories){
+    for (var c in categories) {
       c.fillContractTypeItems();
     }
 
@@ -51,25 +50,40 @@ class Category {
 
   addContractType({Available available}) {
     if (!_containsContractType(available.contractType)) {
-      contractTypes.add(ContractType(name: available.contractType, displayName: available.contractDisplay, position: typesPosition[available.contractType], categoryDisplayName: available.contractCategoryDisplay));
+      var contractType = ContractType(available: available,
+          name: available.contractType,
+          displayName: available.contractDisplay,
+          categoryDisplayName: available.contractCategoryDisplay);
+      contractTypes.add(contractType);
     }
   }
 
   bool _containsContractType(String name) {
     for (var c in contractTypes) {
-      if (c.name == name){
+      if (c.name == name) {
         return true;
       }
     }
     return false;
   }
 
-  fillContractTypeItems(){
+  fillContractTypeItems() {
     if (contractTypes.length % 2 == 0) {
       for (int i = 0; i < contractTypes.length; i += 2) {
-        contractTypeItems.add(ContractTypeItem(
-            contractTypes[i].displayName, contractTypes[i + 1].displayName, "${contractTypes[i].displayName} ${contractTypes[i + 1].displayName}", contractTypes[i].categoryDisplayName));
+        var contractItem = ContractTypeItem(
+            contractTypes[i].displayName, contractTypes[i + 1].displayName,
+            "${contractTypes[i].displayName} ${contractTypes[i + 1]
+                .displayName}", contractTypes[i].categoryDisplayName);
 
+        switch (contractTypes[i].available.contractCategory) {
+          case 'digits':
+            contractItem = DigitsContractItem(contractItem: contractItem, lastDigitRanges: contractTypes[i].available.lastDigitRange);
+            break;
+          default:
+            break;
+        }
+
+        contractTypeItems.add(contractItem);
       }
     }
   }
@@ -80,12 +94,14 @@ class ContractType {
   final String name;
   final String displayName;
   final categoryDisplayName;
-  final Position position;
 
-  ContractType({this.categoryDisplayName, this.name, this.displayName, this.position});
+  final Available available;
+
+  ContractType(
+      {this.available, this.categoryDisplayName, this.name, this.displayName});
 }
 
-class ContractTypeItem{
+class ContractTypeItem {
   final String displayName;
   final String categoryName;
   final String top;
@@ -94,42 +110,52 @@ class ContractTypeItem{
   ContractTypeItem(this.top, this.bottom, this.displayName, this.categoryName);
 }
 
+class DigitsContractItem extends ContractTypeItem {
+  final List<int> lastDigitRanges;
+
+  DigitsContractItem(
+      {ContractTypeItem contractItem, this.lastDigitRanges})
+      : super(contractItem.top, contractItem.bottom, contractItem.displayName, contractItem.categoryName);
+}
+
 const typesPosition = {
-  'CALL'        : Position.TOP,
-  'PUT'         : Position.BOTTOM,
-  'CALLE'       : Position.TOP,
-  'PUTE'        : Position.BOTTOM,
-  'ASIANU'      : Position.TOP,
-  'ASIAND'      : Position.BOTTOM,
-  'DIGITMATCH'  : Position.TOP,
-  'DIGITDIFF'   : Position.BOTTOM,
-  'DIGITEVEN'   : Position.TOP,
-  'DIGITODD'    : Position.BOTTOM,
-  'DIGITOVER'   : Position.TOP,
-  'DIGITUNDER'  : Position.BOTTOM,
+  'CALL': Position.TOP,
+  'PUT': Position.BOTTOM,
+  'CALLE': Position.TOP,
+  'PUTE': Position.BOTTOM,
+  'ASIANU': Position.TOP,
+  'ASIAND': Position.BOTTOM,
+  'DIGITMATCH': Position.TOP,
+  'DIGITDIFF': Position.BOTTOM,
+  'DIGITEVEN': Position.TOP,
+  'DIGITODD': Position.BOTTOM,
+  'DIGITOVER': Position.TOP,
+  'DIGITUNDER': Position.BOTTOM,
   'EXPIRYRANGEE': Position.TOP,
-  'EXPIRYMISSE' : Position.BOTTOM,
-  'EXPIRYRANGE' : Position.TOP,
-  'EXPIRYMISS'  : Position.BOTTOM,
-  'RANGE'       : Position.TOP,
-  'UPORDOWN'    : Position.BOTTOM,
-  'ONETOUCH'    : Position.TOP,
-  'NOTOUCH'     : Position.BOTTOM,
-  'LBFLOATCALL' : Position.MIDDLE,
-  'LBFLOATPUT'  : Position.MIDDLE,
-  'LBHIGHLOW'   : Position.MIDDLE,
-  'RESETCALL'   : Position.TOP,
-  'RESETPUT'    : Position.BOTTOM,
-  'CALLSPREAD'  : Position.TOP,
-  'PUTSPREAD'   : Position.BOTTOM,
-  'TICKHIGH'    : Position.TOP,
-  'TICKLOW'     : Position.BOTTOM,
-  'RUNHIGH'     : Position.TOP,
-  'RUNLOW'      : Position.BOTTOM,
+  'EXPIRYMISSE': Position.BOTTOM,
+  'EXPIRYRANGE': Position.TOP,
+  'EXPIRYMISS': Position.BOTTOM,
+  'RANGE': Position.TOP,
+  'UPORDOWN': Position.BOTTOM,
+  'ONETOUCH': Position.TOP,
+  'NOTOUCH': Position.BOTTOM,
+  'LBFLOATCALL': Position.MIDDLE,
+  'LBFLOATPUT': Position.MIDDLE,
+  'LBHIGHLOW': Position.MIDDLE,
+  'RESETCALL': Position.TOP,
+  'RESETPUT': Position.BOTTOM,
+  'CALLSPREAD': Position.TOP,
+  'PUTSPREAD': Position.BOTTOM,
+  'TICKHIGH': Position.TOP,
+  'TICKLOW': Position.BOTTOM,
+  'RUNHIGH': Position.TOP,
+  'RUNLOW': Position.BOTTOM,
 };
 
 enum Position {
-  TOP, BOTTOM, MIDDLE
+  TOP,
+  BOTTOM,
+  MIDDLE
 }
 
 
