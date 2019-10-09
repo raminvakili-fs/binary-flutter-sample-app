e0c3b00faefba6541ae7be7aa981d26947ab5273
Ramin <ramin.vakili@firstsource.io>
2019-10-09 16:40:47 +0330

    starting web server

--- lib/screens/trade_screen.dart
+++ lib/screens/trade_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:binary_mobile_app/app_constants.dart';
 import 'package:binary_mobile_app/screens/statement_screen.dart';
 import 'package:binary_mobile_app/viewmodels/trade_view_model.dart';
 import 'package:binary_mobile_app/widgets/contracts_type_widget.dart';
@@ -6,9 +10,14 @@ import 'package:binary_mobile_app/widgets/price_proposal_widget.dart';
 import 'package:binary_mobile_app/widgets/shared/binary_progress_indicator.dart';
 import 'package:binary_mobile_app/widgets/symbols_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:http_server/http_server.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TradeScreen extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -28,6 +37,43 @@ class TradeScreen extends StatelessWidget {
                   return StatementScreen();
                 }));
               },
+            ),
+
+            IconButton(
+              icon: Icon(Icons.assignment_ind),
+              onPressed: () async {
+
+                runZoned((){
+
+                  HttpServer.bind('0.0.0.0', 80).then((server){
+
+                    print('*************************** Server running at: ${server.address.address}');
+
+                    server.transform(HttpBodyHandler()).listen((HttpRequestBody body) async {
+
+                      print('**************************************** ${body.request.uri}');
+
+
+                    });
+
+                    server.listen((HttpRequest request) {
+                      print('*************************** ${request.first}');
+                    });
+
+                  });
+                });
+
+                var url = 'https://oauth.binary.com/oauth2/authorize?app_id=$APP_ID';
+
+
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+
+
+              }
             )
           ],
         ),
@@ -35,7 +81,9 @@ class TradeScreen extends StatelessWidget {
       ),
     );
   }
+
 }
+
 
 class TradeView extends StatefulWidget {
   @override
