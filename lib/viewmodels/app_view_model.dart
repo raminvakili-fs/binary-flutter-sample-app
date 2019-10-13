545cea84ceac29855f3556897cd34b5883bc77d7
Ramin <ramin.vakili@firstsource.io>
2019-10-13 13:49:46 +0330

    adding authenticated user info to App view model stream

--- lib/viewmodels/app_view_model.dart
+++ lib/viewmodels/app_view_model.dart
@@ -4,24 +4,21 @@ import 'package:rxdart/rxdart.dart';
 import 'package:binary_mobile_app/model/authentication/oauth.dart';
 
 class AppViewModel extends ChangeNotifier {
-  
-  
 
   final _userInfo = BehaviorSubject<User>();
+
   BehaviorSubject<User> get userInfo => _userInfo;
   
   authenticateWithOauth() async{
     User user = await OAuthentication.instance.getToken();
     print('User info: ${user.accounts[0].token}');
-
+    _userInfo.add(user);
   }
-  
-  
+
   @override
   void dispose() {
     super.dispose();
     _userInfo.close();
   }
-
 
 }
